8a2a15898a429b276439a2f108c7085f0aafc2c1
PritamShakyawal <pritam9433skw@gmail.com>
2024-10-28 23:38:57 +0530

    commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,60 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import './Card.css';
 import logo from './logo.svg';
-import priority from './Priority.svg'; // Make sure the file name is correct
-import tag from './tag.png';
-import img0 from './nopriority.png';
-import img4 from './urgent.png';
-import img3 from './high.png';
-import img2 from './medium.png';
-import img1 from './low.png';
-import done from './Done.png';
-import Cancelled from './canceled.png';
-import backlogimg from './backlog.png';
-import inprogressimg from './in progress.png';
-import todo from './to do.png';
-import usr1 from './usr-1.png';
-import usr2 from './usr-2.png';
-import usr3 from './usr-3.png';
-import usr4 from './usr-4.png';
-import usr5 from './usr-5.png';
-import usr6 from './usr-6.png';
-import usr7 from './usr-7.png';
+import priority from './Priority.svg';
+import tag from './tag.png'
+import img0 from './nopriority.png'
+import img4 from './urgent.png'
+import img3 from './high.png'
+import img2 from './medium.png'
+import img1 from './low.png'
+import done from './Done.png'
+import Cancelled from './canceled.png'
+import backlogimg from './backlog.png'
+import inprogressimg from './in progress.png'
+import todo from './to do.png'
+import usr1 from './usr-1.png'
+import usr2 from './usr-2.png'
+import usr3 from './usr-3.png'
+import usr4 from './usr-4.png'
+import usr5 from './usr-5.png'
+import usr6 from './usr-6.png'
+import usr7 from './usr-7.png'
+
+// import img from 
 
 const Card = (props) => {
-    const [available, setAvailable] = useState(false);
-    const [users, setUsers] = useState([]);
+    // let available = true;
+    const [available, setavailable] = useState(false);
+    // console.log({props});
+    let imgt=`imgr${props.ticket.priority.toString()}`;
+    // console.log(imgt);
+    let dotuser;
+    
+    const [users, setusers] = useState([]);
+    const [tick, setTick] = useState([]);
 
     useEffect(() => {
-        fetchData();
+
+        hello();
+        // count();
+
+
     }, []);
 
-    async function fetchData() {
+    async function hello() {
         try {
             const response = await fetch("https://api.quicksell.co/v1/internal/frontend-assignment");
+
             const result = await response.json();
-            setUsers(result.users);
+
+        setTick(result.tickets);
+        setusers(result.users);
         } catch (error) {
             console.error("Error:", error);
         }
-    }
 
+
+    }
     const priorityImageMap = {
         0: img0,
         1: img1,
         2: img2,
         3: img3,
         4: img4,
-    };
-
-    const statusImageMap = {
+      };
+    const statusImageMap={
         "Todo": todo,
-        "In progress": inprogressimg,
-        "Backlog": backlogimg,
-        "Done": done,
-        "Cancelled": Cancelled,
-    };
+        "In progress":inprogressimg,
+        "Backlog":backlogimg,
+        "Done":done,
+        "Cancelled":Cancelled,
 
+    }
     const usrImageMap = {
         "usr-1": usr1,
         "usr-2": usr2,
@@ -63,39 +79,53 @@ const Card = (props) => {
         "usr-5": usr5,
         "usr-6": usr6,
         "usr-7": usr7,
-    };
+      };
+      useEffect(() => {
+        users.map((user) => {
+                               
+            if(
+                props.ticket &&
+                 user.id === props.ticket.userId){
+                    setavailable(user.available);
+                
+            }  })   
+      }, [users])
+      
+      
+      const usrImage=usrImageMap[props.ticket.userId]||usr1;
+      const imgSrc = priorityImageMap[props.ticket.priority] || img0;
+      const statusImgSrc=statusImageMap[props.ticket.status]||todo;
+    if(available===true){
+        dotuser=<div className='availableUser' />;
 
-    useEffect(() => {
-        if (props.ticket) {
-            const user = users.find(user => user.id === props.ticket.userId);
-            if (user) {
-                setAvailable(user.available);
-            }
-        }
-    }, [users, props.ticket]);
-
-    const usrImage = usrImageMap[props.ticket.userId] || usr1;
-    const imgSrc = priorityImageMap[props.ticket.priority] || img0;
-    const statusImgSrc = statusImageMap[props.ticket.status] || todo;
+    }else{
+        dotuser=<div className='notavailableUser' />;
+    }
+   
+    
 
     return (
         <div className='cardBox'>
             <div className='cardBoxrow'>
                 <div className='cardBoxin'>
-                    <span className='cardId'>{props.ticket.id}</span>
-                    <span className='cardTitle'><img src={statusImgSrc} alt="" />{props.ticket.title}</span>
-                </div>
+                    <text className='cardId'>{props.ticket.id}</text>
+                    <text className='cardTitle'><img  src={statusImgSrc} ></img>{props.ticket.title}</text>
+                </div >
                 <div style={{ height: "38px" }}>
                     <img className='userImg' src={usrImage} alt='' />
-                    {available ? <div className='availableUser' /> : <div className='notavailableUser' />}
+                    {dotuser}
                 </div>
             </div>
+
+
             <div className='lowerBox'>
-                <div className='priorityBox'><img className='priorityImg' src={imgSrc} alt='priority' /></div>
+                <div className='priorityBox'><img className='priorityImg' src={imgSrc} alt='logo' /></div>
+
                 <div className='tagBox'>
-                    <img className='tagImg' src={tag} alt='tag' />
-                    <span className='tagText'>{props.ticket.tag}</span>
+                    <img className='tagImg' src={tag} alt='logo' />
+                    <tag className='tagText'>{props.ticket.tag}</tag>
                 </div>
+
             </div>
         </div>
     );
